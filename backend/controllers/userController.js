39d96bdde14d7cbad75d1873a43460e7cb0c69e6
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2022-02-05 17:21:38 +0100

    Connect DB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
 
 // @desc  Register a new user
 // @route /api/users
@@ -11,7 +14,38 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please Include all fields')
   }
-  res.send('Register Route')
+
+  // Find if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+
+
 })
 
 // @desc  Login a user
