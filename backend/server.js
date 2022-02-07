7929ca6e5394ffea0f7b2dc033e5a5d6983ca9fe
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2022-02-07 09:10:00 +0100

    jwt/private routes added and frontend initialized

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
