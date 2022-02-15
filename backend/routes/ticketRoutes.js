a806a1daf8fb0225b7020fd564b2845e3f8ff1a7
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2022-02-15 20:24:14 +0100

    BE: Notes

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,6 +4,9 @@ const { getTickets, createTicket, getTicket, updateTicket, deleteTicket } = requ
 
 const { protect } = require('../middleware/authMiddleware')
 
+// Re- route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
